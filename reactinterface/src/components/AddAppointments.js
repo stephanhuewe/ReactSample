218132c8a9fb5aa281fc8a240bd4b18764549686
Stephan Hüwe <sh@huestel.de>
2020-05-31 13:54:19 +0200

    Finished

--- reactinterface/src/components/AddAppointments.js
+++ reactinterface/src/components/AddAppointments.js
@@ -1,8 +1,174 @@
 import React, { Component } from 'react';
+import {FaPlus} from 'react-icons/fa';
 
 class AddAppointments extends Component {
+
+  constructor() { 
+      super();
+      this.state = {
+      petName : '',
+      ownerName: '',
+      aptDate: '',
+      aptTime: '',
+      aptNotes: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(e)
+  {
+    e.preventDefault();
+    let tempApt = {
+      petName: this.state.petName,
+      ownerName: this.state.ownerName,
+      aptDate: this.state.aptDate + " " + this.state.aptTime,
+      aptNotes: this.state.aptNotes
+    };
+
+    this.props.AddAppointment(tempApt);
+
+    this.setState({
+        petName: "",
+        ownerName: "",
+        aptDate: "",
+        aptTime: "",
+        aptNotes: ""
+    });
+
+    this.props.toggleForm();
+    
+  }
+
+  handleChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState(
+      {
+        [name] : value
+      }
+    );
+  }
+
   render() {
-    return <div>Termin vereinbaren</div>;
+    return (
+      <div className={
+        'card textcenter mt-3 ' +
+        (this.props.formDisplay ? '' : 'add-appointment')
+      }>
+      <div className="apt-addheading card-header bg-primary text-white" onClick={this.props.toggleForm}>
+       <FaPlus></FaPlus>  Termin hinzufügen
+      </div>
+
+      <div className="card-body">
+        <form id="aptForm" noValidate onSubmit={this.handleAdd}>
+          <div className="form-group form-row">
+            <label
+              className="col-md-2 col-form-label text-md-right"
+              htmlFor="petName"
+              readOnly
+            >
+              Tiername
+            </label>
+            <div className="col-md-10">
+              <input
+                type="text"
+                className="form-control"
+                name="petName"
+                placeholder="Tiername"
+                value={this.state.petName}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group form-row">
+            <label
+              className="col-md-2 col-form-label text-md-right"
+              htmlFor="ownerName"
+            >
+              Besitzer
+            </label>
+            <div className="col-md-10">
+              <input
+                type="text"
+                className="form-control"
+                name="ownerName"
+                placeholder="Name des Besitzers"
+                value={this.state.ownerName}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group form-row">
+            <label
+              className="col-md-2 col-form-label text-md-right"
+              htmlFor="aptDate"
+            >
+              Datum
+            </label>
+            <div className="col-md-4">
+              <input
+                type="date"
+                className="form-control"
+                name="aptDate"
+                id="aptDate"
+                value={this.state.aptDate}
+                onChange={this.handleChange}
+              />
+            </div>
+            <label
+              className="col-md-2 col-form-label text-md-right"
+              htmlFor="aptTime"
+            >
+              Uhrzeit
+            </label>
+            <div className="col-md-4">
+              <input
+                type="time"
+                className="form-control"
+                name="aptTime"
+                id="aptTime"
+                value={this.state.aptTime}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group form-row">
+            <label className="col-md-2 text-md-right" htmlFor="aptNotes">
+              Anmerkungen
+            </label>
+            <div className="col-md-10">
+              <textarea
+                className="form-control"
+                rows="4"
+                cols="50"
+                name="aptNotes"
+                id="aptNotes"
+                placeholder="Anmerkungen"
+                value={this.state.aptNotes}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group form-row mb-0">
+            <div className="offset-md-2 col-md-10">
+              <button
+                type="submit"
+                className="btn btn-primary d-block ml-auto"
+              >
+                Termin hinzufügen
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    );
   }
 }
 
--- reactinterface/src/components/SearchAppointments.js
+++ reactinterface/src/components/SearchAppointments.js
@@ -2,7 +2,91 @@ import React, { Component } from 'react';
 
 class SearchAppointments extends Component {
   render() {
-    return <div>Termine suchen</div>;
+
+   
+
+
+    return (
+      <div className="search-appointments row justify-content-center my-4">
+        <div className="col-md-6">
+          <div className="input-group">
+            <input
+              id="SearchApts"
+              type="text"
+              className="form-control"
+              aria-label="Termine durchsuchen"
+              onChange = {e => this.props.searchApts(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button
+                type="button"
+                className="btn btn-primary dropdown-toggle"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Sortieren nach: <span className="caret" />
+              </button>
+
+              <div className="sort-menu dropdown-menu dropdown-menu-right">
+                <button className={
+                    'sort-by dropdown-item ' +
+                    (this.props.orderBy === 'petName' ? 'active' : '')
+                  }
+                  onClick={e =>
+                    this.props.changeOrder('petName', this.props.orderDir)
+                  }
+                  href="#"
+                >
+                  Tiername
+                </button>
+                <button className={
+                    'sort-by dropdown-item ' +
+                    (this.props.orderBy === 'aptDate' ? 'active' : '')
+                  }
+                  onClick={e =>
+                    this.props.changeOrder('aptDate', this.props.orderDir)
+                  }
+                  href="#">
+                  Datum
+                </button>
+                <button className={
+                    'sort-by dropdown-item ' +
+                    (this.props.orderBy === 'ownerName' ? 'active' : '')
+                  }
+                  onClick={e =>
+                    this.props.changeOrder('ownerName', this.props.orderDir)
+                  }
+                  href="#">
+                  Besitzer
+                </button>
+                <div role="separator" className="dropdown-divider" />
+                <button className={
+                    'sort-by dropdown-item ' +
+                    (this.props.orderDir === 'asc' ? 'active' : '')
+                  }
+                  onClick={e =>
+                    this.props.changeOrder(this.props.orderBy, 'asc')
+                  }
+                  href="#">
+                  aufsteigend
+                </button>
+                <button  className={
+                    'sort-by dropdown-item ' +
+                    (this.props.orderDir === 'desc' ? 'active' : '')
+                  }
+                  onClick={e =>
+                    this.props.changeOrder(this.props.orderBy, 'desc')
+                  }
+                  href="#">
+                  absteigend
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
